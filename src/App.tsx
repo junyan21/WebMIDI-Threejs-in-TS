6935e8f667b6812e0f75f023bc84c3a64db2cb44
Junya <93970227+junyan21@users.noreply.github.com>
2023-04-07 20:20:41 +0900

    Add WebMidi + Threejs in Typescript functionality and MIDI device event listeners to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/// <reference types="webmidi" />
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { useEffect, useRef } from 'react';
+import * as THREE from 'three';
 
-export default App;
+export const App: React.FC = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+
+    if (containerRef.current) {
+      // Three.jsのシーンセットアップ
+      const scene = new THREE.Scene();
+      const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+      const renderer = new THREE.WebGLRenderer();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      containerRef.current.appendChild(renderer.domElement);
+
+      // ここにThree.jsのオブジェクトを追加
+
+      const animate = () => {
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+      }
+
+      animate();
+
+      // Web MIDI APIの設定
+      navigator.requestMIDIAccess()
+        .then((midiAccess: WebMidi.MIDIAccess) => {
+          // 入力デバイスの取得
+          const inputs = Array.from(midiAccess.inputs.values());
+
+          // 入力デバイスにイベントリスナーを追加
+          for (const input of inputs) {
+            input.onmidimessage = (message: WebMidi.MIDIMessageEvent) => {
+              const command = message.data[0];
+              const note = message.data.length > 1 ? message.data[1] : undefined;
+              const velocity = message.data.length > 2 ? message.data[2] : undefined;
+
+              // ノートオンイベント
+              if (command >= 0x90 && command <= 0x9F) {
+                console.log(`Note on: note = ${note}, velocity = ${velocity}`);
+              }
+              // ノートオフイベント
+              else if (command >= 0x80 && command <= 0x8F) {
+                console.log(`Note off: note = ${note}, velocity = ${velocity}`);
+              }
+              // コントロールチェンジイベント
+              if (command >= 0xB0 && command <= 0xBF) {
+                const controllerNumber = note; // コントロール番号
+                const controllerValue = velocity; // コントロール値
+
+                console.log(`Control Change: controllerNumber = ${controllerNumber}, controllerValue = ${controllerValue}`);
+              }
+              // ピッチベンドイベント
+              if (command >= 0xE0 && command <= 0xEF && velocity && note) {
+                const pitchBendValue = (velocity << 7) | note; // ピッチベンド値 (14-bit)
+
+                console.log(`Pitch Bend: pitchBendValue = ${pitchBendValue}`);
+              }
+            };
+          }
+        })
+        .catch(() => {
+          console.error('MIDIデバイスにアクセスできませんでした。');
+        });
+    }
+  }, []);
+
+  return <div ref={containerRef} />;
+};
